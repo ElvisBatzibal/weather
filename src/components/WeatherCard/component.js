96b102fb000719e1d6436d9d9dff3da106067ca5
Elvis Geovanny <elvis.batzibal@hotmail.es>
2019-12-26 20:30:05 -0600

    Defined Card styled.div
    Defined Card styled.div  and delete css to app css ".card"

--- src/components/WeatherCard/component.js
+++ src/components/WeatherCard/component.js
@@ -1,10 +1,22 @@
 import React from 'react'
-import { styled } from '@emotion/styled'
+import styled from '@emotion/styled'
 
 const WeatherCard=(props) =>
 {
+   const Card = styled.div`
+   margin:  0 auto;
+   background: linear-gradient(to bottom, blue, lightblue );
+   width: 200px;
+   height: 280px;
+   display: flex;
+   flex-direction: column;
+   justify-content: space-around ;
+   align-items: center;
+   border-radius: 15px;
+   `
+
     return (
-    <div className="card">
+    <Card className="card">
         <div className="location">
             <h1 className="city">Sydney</h1>
             <h3 className="country">AU</h3>
@@ -12,7 +24,7 @@ const WeatherCard=(props) =>
         <img className="icon" src="./img/icon-weather.png" alt="Weather Icon"></img>
         <h1 className="temp">20 *C</h1>
         <h3 className="condition">Clouds</h3>
-    </div>
+    </Card>
     );
 }
 
